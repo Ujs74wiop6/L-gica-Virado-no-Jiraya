370ae41dc926c32e2a7ff5d31fa68ad246a95d89
Fabricio Tiago Arantes <fabricioujs74@gmail.com>
2022-03-03 21:47:34 -0300

    Atualização: 10/44

---
+++ src/logicaviradonojiraya/CalculadorDiferenceIdade.java
@@ -0,0 +1,26 @@
+package logicaviradonojiraya;
+
+/**
+ *
+ * @author Fabricio
+ */
+/**
+ * Criar uma classe chamada CalculadorDiferenceIdade Criar uma variável para
+ * guardar a primeira idade Criar uma variável para guardar a segunda idade
+ * Criar uma variável para guardar a diferença entra a idade 1 e a idade 2
+ * Imprima a diferença no console
+ */
+public class CalculadorDiferenceIdade {
+
+    public static void main(String[] args) {
+
+        int idade1 = 30;
+        int idade2 = 13;
+        int diferenca;
+
+        System.out.println("Primeira idade: " + idade1);
+        System.out.println("Primeira idade: " + idade2);
+        diferenca = (idade1 - idade2);
+        System.out.println("A diferença de idades é: " + diferenca);
+    }
+}
--- src/logicaviradonojiraya/LogicaViradoNoJiraya.java
+++ src/logicaviradonojiraya/LogicaViradoNoJiraya.java
@@ -6,7 +6,15 @@ package logicaviradonojiraya;
  */
 public class LogicaViradoNoJiraya {
 
+    //Max int value JAVA: {-2147483648 to 2147483647}
+    //Max byte value JAVA: {127}
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("Hello development world");
+        System.out.println("Vamos começar a Maratona de Lógica com o DevDojo");
+
+        int idade = 18;
+
+        System.out.println("Minha idade: " + idade);
     }
 }
