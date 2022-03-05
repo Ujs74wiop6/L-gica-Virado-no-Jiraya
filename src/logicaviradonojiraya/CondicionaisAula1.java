f6c664fbdee81b3777bad4751d8e4300161930ee
Fabricio Tiago Arantes <fabricioujs74@gmail.com>
2022-03-05 19:09:28 -0300

    Atualização: 05/03/2022

---
+++ src/logicaviradonojiraya/CondicionaisAula1.java
@@ -0,0 +1,20 @@
+package logicaviradonojiraya;
+
+/**
+ *
+ * @author Fabricio
+ */
+public class CondicionaisAula1 {
+
+    public static void main(String[] args) {
+
+        boolean condicao = false;
+
+//O if só executa uma ação quando verdadeira
+        if (condicao) {
+            System.out.println("Dentro do 'IF'");
+        } else {
+            System.out.println("Dentro do 'ELSE'");
+        }
+    }
+}
---
+++ src/logicaviradonojiraya/VerificaIdade.java
@@ -0,0 +1,19 @@
+package logicaviradonojiraya;
+
+/**
+ *
+ * @author Fabricio
+ */
+public class VerificaIdade {
+
+    public static void main(String[] args) {
+        String nome = "Ana Carolina";
+        int idade = 18;
+//        {<, >, ==, =!,} operadores básicos de operação no if-else
+        if (idade >= 18) {
+            System.out.println("O Sr." + nome + " é maior de idade");
+        } else {
+            System.out.println("O Sr." + nome + " é menor de idade");
+        }
+    }
+}
